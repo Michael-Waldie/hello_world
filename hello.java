c9deb2141827c661de91fedf8c96c03c824b8505
Michael Waldie <michaelwaldie26196@gmail.com>
2023-03-22 02:29:58 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,6 +5,7 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
+	   // When I put my name as it said in the instructions it said the greeting was not in the right format or missing.
        String firstName = "Alex";
 	
        // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
